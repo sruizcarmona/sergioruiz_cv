d850be98388cb4e5b6b2f11fb11ea1e1ca84f20e
sruizcarmona <9433769+sruizcarmona@users.noreply.github.com>
2026-06-24 09:35:51 +0200

    clean branch

--- resume_2pages/update_docs.sh
+++
@@ -1,5 +0,0 @@
-cp resume.pdf resume.html ../docs
-cp resume.html ../docs/index.html
-git add .* ../docs/*
-git commit -m 'update cv'
-git push
--- update_docs.sh
+++
@@ -1,4 +0,0 @@
-cp cv.pdf docs/full.pdf
-cp cv.html docs/full.html
-cp resume_2pages/resume.html resume_2pages/resume.pdf docs
-cp docs/resume.html docs/index.html
